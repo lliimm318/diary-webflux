39321bebd14f3eb327a1703b05adf2e013758648
seoyooung <66578746+lliimm318@users.noreply.github.com>
2022-10-28 17:53:47 +0900

    [UPDATE] 안쓰는 import 지우기

--- src/main/java/com/study/webflux/repository/UserRepository.java
+++ src/main/java/com/study/webflux/repository/UserRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 
-import java.util.Optional;
-
 @Repository
 public interface UserRepository extends ReactiveCrudRepository<User, String> {
 
