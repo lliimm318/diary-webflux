464313bf2b9cd80b0078ab8269d0b89813ed1d1b
seoyooung <66578746+lliimm318@users.noreply.github.com>
2022-10-28 13:13:00 +0900

    [ADD] user controller

---
+++ src/main/java/com/study/webflux/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.study.webflux.controller;
+
+import com.study.webflux.domain.service.User.UserService;
+import com.study.webflux.payload.request.AuthRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public Mono register(@Valid @RequestBody AuthRequest request) {
+        return userService.register(request);
+    }
+
+    @PostMapping("/auth")
+    public Mono login(@Valid @RequestBody AuthRequest request) {
+        return userService.login(request);
+    }
+
+
+
+}
