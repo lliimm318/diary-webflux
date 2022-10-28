b588628f3103f0c8c56fc55094c97003794f4e6b
seoyooung <66578746+lliimm318@users.noreply.github.com>
2022-10-28 13:15:41 +0900

    [ADD] diary repository

---
+++ src/main/java/com/study/webflux/repository/DiaryRepository.java
@@ -0,0 +1,20 @@
+package com.study.webflux.repository;
+
+import com.study.webflux.entity.Diary;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+@Repository
+public interface DiaryRepository extends ReactiveCrudRepository<Diary, UUID> {
+
+    Flux<Diary> findByWriterOrderByDate(String writer);
+
+    Flux<Diary> findByIsLockedFalseOrderByDate();
+
+    Mono<Diary> findByIdAndIsLockedFalse(UUID uuid);
+
+}
