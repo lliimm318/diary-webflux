810b1141f63c18ce254c345993e1abfa68bf580c
seoyooung <66578746+lliimm318@users.noreply.github.com>
2022-10-31 11:06:40 +0900

    [UPDATE]

--- src/main/java/com/study/webflux/domain/service/User/AuthFacade.java
+++ src/main/java/com/study/webflux/domain/service/User/AuthFacade.java
@@ -1,18 +1,13 @@
 package com.study.webflux.domain.service.User;
 
-import com.study.webflux.entity.User;
-import com.study.webflux.exception.UserNotFoundException;
-import com.study.webflux.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import reactor.core.publisher.Mono;
 
 @Component
 @RequiredArgsConstructor
 public class AuthFacade {
-    private final UserRepository userRepository;
 
     public Authentication getAuthentication() {
         return SecurityContextHolder.getContext().getAuthentication();
