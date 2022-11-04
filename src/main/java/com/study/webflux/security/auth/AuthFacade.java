7f200dc8af533012d277587c190db925a48ab7c4
seoyooung <66578746+lliimm318@users.noreply.github.com>
2022-11-04 14:58:21 +0900

    [FIX] 파일구조 변경

--- src/main/java/com/study/webflux/security/auth/AuthFacade.java
+++ src/main/java/com/study/webflux/security/auth/AuthFacade.java
@@ -1,5 +1,7 @@
-package com.study.webflux.domain.service.User;
+package com.study.webflux.security.auth;
 
+import com.study.webflux.entity.User;
+import com.study.webflux.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -9,11 +11,17 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class AuthFacade {
 
+    private final UserRepository userRepository;
+
     public Authentication getAuthentication() {
         return SecurityContextHolder.getContext().getAuthentication();
     }
 
     public String getUserName() {
         return this.getAuthentication().getName();
+    }
+
+    public User getUser(String name) {
+        return userRepository.findByName(name).block();
     }
 }
